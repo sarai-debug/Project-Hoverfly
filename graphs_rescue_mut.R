ebc2f170e9a1df6734237cc11293ccd90fa194b2
charith <charith.meegamuwage@students.unibe.ch>
2025-10-23 08:54:58 +0200

    Update graphs_rescue_mut.R

--- graphs_rescue_mut.R
+++ graphs_rescue_mut.R
@@ -1,13 +1,18 @@
 num_rounds <- 400
-hatching_rate <- seq(0, 2, by = 0.05)
+inita_pop <- 1000+50
+max_gen <- 200
+
+hZ_m_values <- seq(0.1, 0.9 , by = 0.1)
 
 
 # creating a data frame to keep all output
 
-results_hm <- data.frame (array(NA, dim =c(0,3), dimnames = list(c(), c("Hatching_rate", "mutant"))))
+results <- data.frame(hZ_m = numeric(), prob_rescue = numeric(), prob_extinct = numeric())
+
+
 
 # for (i in 1:num_rounds)   
-for (mut in mut_rates) {
+for (hZ_m in hZ_m_values) {
   
   extinct_count <- 0
   rescue_count <- 0
@@ -16,8 +21,8 @@ for (mut in mut_rates) {
     
     
     sim <- simulate_pop(Z_init_w = 100, Z_init_m = 50, f_w = 3, f_m = 3, 
-                        hZ_w = 0.5, hZ_m = 0.5, pZ_w = 0.5, pZ_m = 0.5, 
-                        mut_rate = mut_rates,t_max = max_gen)
+                        hZ_w = 0.5, hZ_m = hZ_m, pZ_w = 0.5, pZ_m = 0.5, 
+                        mut_rate = 0.001, t_max = max_gen)
     
     # total_pop <- round_gen[dim(round_gen)[1],1] + round_gen[dim(round_gen)[1],2]
     total_pop <- sum(tail(sim, 1))
@@ -30,41 +35,42 @@ for (mut in mut_rates) {
     }
     else{
       rescue_count <- rescue_count + 1
-      freq_df <- rbind(freq_df, c(mut, sim[nrow(sim),1]/total_pop, sim[nrow(sim),2]/total_pop))
+      # freq_df <- rbind(freq_df, c(mut, sim[nrow(sim),1]/total_pop, sim[nrow(sim),2]/total_pop))
     }
   }
   
   # result for each mutation rate
-  results_hm <- rbind(results_hm, data.frame(mut_rates = mut, 
+  results <- rbind(results, data.frame(hZ_m = hZ_m, 
                                        prob_rescue = rescue_count/num_rounds * 100, 
                                        prob_extinct = extinct_count/num_rounds * 100))
 }
-print(results_hm)
+print(results)
 
-colnames(freq_df) <- c("Hatching_rate", "wildtype", "mu")
-print(freq_df)
+# # colnames(freq_df) <- c("hZ_m", "wildtype", "mutant")
+# print(head(freq_df))
 
 # plot for rescue probability
 
-plot(results_hm$mut_rates, results_hm$prob_rescue, type='b',  
-     col='darkgreen', pch=21, xlab='Mutation rate', 
+plot(results$hZ_m, results$prob_rescue, type='b',  
+     col='darkgreen', pch=21, xlab='Mutant hatching rate', 
      ylab='Rescue probability (%)',
-     main='Evolutionary Rescue vs Mutation Rate')
+     main='Evolutionary Rescue vs Mutant hatching Rate')
 
-# frequency plot against mutation rate
+# # frequency plot against mutant hatching rate
+# 
+# plot(freq_df$hZ_m, freq_df$mutant, type='p',  
+#      col='darkgreen', pch=21, xlab='Mutant hatching rate', 
+#      ylab='Frequency of Mutant at End of Simulation',
+#      main='Frequency at Rescue vs Mutant hatching Rate')
 
-plot(freq_df$mutation_rate, freq_df$mutant, type='p',  
-     col='darkgreen', pch=21, xlab='Mutation rate', 
-     ylab='Frequency of Mutant at End of Simulation',
-     main='Frequency at Rescue vs Mutation Rate')
 
-# boxplot for frequency and mutation rate
-boxplot(mutant ~ mutation_rate,
-        data = freq_df,
-        col = "darkgreen",
-        border = "black",
-        xlab = "Mutation rate",
-        ylab = "Frequency of Mutant at End of Simulation",
-        main = "Frequency at Rescue vs Mutation Rate")
+# # boxplot for frequency and mutant hatching rate
+# boxplot(mutant_freq ~ hZ_m,
+#         data = freq_df,
+#         col = "darkgreen",
+#         border = "black",
+#         xlab = "Mutant hatching rate",
+#         ylab = "Frequency of Mutant at End of Simulation",
+#         main = "Distribution of mutant frequency at rescue")
 
 
